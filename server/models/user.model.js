a78ee4a84c7cb7ba61f181097a51f99cd6ef8cd4
Shibani15 <mishrashibani384@gmail.com>
2020-02-13 09:54:18 +0530

    [Naresh] Added. models for chat app

---
+++ server/models/user.model.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+let Schema = mongoose.Schema;
+var emailExistence = require('email-existence')
+const bcrypt = require('bcrypt');
+
+const userData = new Schema({
+    firstName:{
+        type:String,
+        required:true
+    },
+    lastName:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    country:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    }
+})
+let register = mongoose.model("users", userData)
+
+exports.Register  = (request, callback) => {
+    emailExistence.check(request.body.email, (err, result) => {
+        if (result) {
+            register.findOne({ "email": request.body.email }, (err, data) => {
+                if (data) callback("Existing");
+                else {
+                    bcrypt.hash(request.body.password, 10, (err, encrypted) => {
+                        var userDetails = new register({
+                            "firstName": request.body.firstName,
+                            "lastName": request.body.lastName,
+                            "email": request.body.email,
+                            "country": request.body.country,
+                            "password": encrypted
+                        })
+                        userDetails.save((err, data) => {
+                            if (err) {
+                                callback(err);
+                            } else callback(null, data);
+                        })
+                    })
+                } 
+                console.log("model end")
+            })
+        }
+        else {
+            callback("email is invalid ")
+        }
+    })
+}
+//exports login
+exports.Login = (request, callback) => {
+    register.findOne({
+        "email": request.body.email
+    }, (err, data) => {
+        if (data) {
+            //compare the password
+            bcrypt.compare(request.body.password, data.password, (err, sucess) => {
+                if (sucess)
+                    callback(null, data);
+                else
+                    callback("Wrong Password");
+            })
+        }
+        else callback("email doesnot match or exist");
+    })
+}
