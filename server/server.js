267d98ab676f0da0d9b2c90090f4bf6784a7cc62
NareshPatilBridglabz <nareshpatil.nrp@gmail.com>
2020-02-22 13:15:14 +0530

    [Naresh] Refactore. add comments

--- server/server.js
+++ server/server.js
@@ -6,19 +6,21 @@
 * @since          : 13-02-2020
 ******************************************************************************/
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const routes = require('./routes/user.routes')
-const expressValidator = require('express-validator')
-const dbconfig = require('./config/database.config')
+const routes = require('./routes/user.routes');
+const expressValidator = require('express-validator');
+const dbconfig = require('./config/database.config');
 const mongoose = require('mongoose');
-const cors = require('cors')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(expressValidator());
 app.use(cors())
+app.use(cookieParser());
 app.use('/', routes);
 
 //connecting to the database
@@ -32,6 +34,7 @@ mongoose.connect(dbconfig.url, {
         console.log('Could not connect to the database');
         process.exit();
     })
+//default route.
 app.get('/', (req, res) => {
     res.json({"message": "Welcome to Chat-App application."});
 });
