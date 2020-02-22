7e28fab81ee5b8f1d32dfc7f56761c46344491bb
NareshPatilBridglabz <nareshpatil.nrp@gmail.com>
2020-02-22 13:20:44 +0530

    [Naresh] Refactore. changes in save services.

--- server/services/user.services.js
+++ server/services/user.services.js
@@ -1,6 +1,8 @@
 const model = require('../models/user.model')
 //export register function
 exports.register = (request, callback) => {
+    console.log("in services ", request.body);
+
     try {
         model.Register(request, (err, data) => {
             if (err) {
@@ -41,15 +43,58 @@ exports.forgotpassword = (request, callback) => {
     }
 }
 //exports reset password
-exports.resetPassword=(request,callback)=>{
-    try{
-    model.ResetPassword(request,(err,data)=>{
-        if(err){
-            callback(err);
-        }else
-        callback(null,data)
-    })
-} catch (e) {
-    console.log(e);
+exports.resetPassword = (request, callback) => {
+    try {
+        model.ResetPassword(request, (err, data) => {
+            if (err) {
+                callback(err);
+            } else
+                callback(null, data)
+        })
+    } catch (e) {
+        console.log(e);
+    }
 }
+//GEt All user data.
+exports.userData = (request, callback) => {
+    try {
+        model.userData(request, (err, data) => {
+            if (err) {
+                callback(err);
+            } else
+                callback(null, data)
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+//Save Chat Services.
+exports.saveChat = (request, callback) => {
+    try {
+        model.saveChat(request, (err, data) => {
+            if (err) {
+                console.log("service ERRO : "+ err);
+                callback(err);
+            } else{
+                callback(null, data)
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+exports.getChat = (request, callback) => {
+    try {
+        model.getChat(request, (err, data) => {
+            if (err) {
+                console.log("service ERRO : "+ err);
+                callback(err);
+            } else{
+                callback(null, data)
+            }
+        })
+    } catch (e) {
+        console.log(e);
+    }
 }
